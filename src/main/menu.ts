6cea3f33e2bd73a0fe54be37f3c37487cd6f33b8
Dane Roelofs <dane.l.roelofs@gmail.com>
2024-10-26 22:44:00 -0700

    chore: :wrench: replace boilerplate code

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Active Pixel',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Active Pixel',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
