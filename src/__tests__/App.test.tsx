4ef16a3dbbaf61156b978dea0f654f868c02e98a
Dane Roelofs <dane.l.roelofs@gmail.com>
2024-10-30 21:36:56 -0700

    test: :white_check_mark: mock window object

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,16 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
+// Mock window.electron before tests
+Object.defineProperty(window, 'electron', {
+  value: {
+    ipcRenderer: {
+      onSuggestionNotification: jest.fn(),
+    },
+  },
+  writable: true,
+});
+
 describe('App', () => {
   it('should render', () => {
     expect(
