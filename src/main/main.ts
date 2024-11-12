6bde2a19c43895bd61c24259f12a2b7c23e666e5
Dane Roelofs <dane.l.roelofs@gmail.com>
2024-11-12 15:10:37 -0800

    chore: :wrench: add min app height and width

--- src/main/main.ts
+++ src/main/main.ts
@@ -134,6 +134,8 @@ const createWindow = async () => {
     show: false,
     width: 1920,
     height: 1080,
+    minWidth: 760,
+    minHeight: 500,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
